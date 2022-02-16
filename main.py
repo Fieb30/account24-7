f09219581ec05d1abefd44a7842ec88f0213029d
Fieb30 <99597945+Fieb30@users.noreply.github.com>
2022-02-17 00:00:37 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+import discord, os, keep_alive, asyncio, datetime, pytz
+
+from discord.ext import tasks, commands
+
+client = commands.Bot(command_prefix=':', self_bot=True)
+
+
+
+keep_alive.keep_alive()
+client.run(os.getenv("TOKEN"), bot=False)
